19ebcd4ad6e73b24d12b23fba890f930f60f9d2a
Motko222 <77466238+Motko222@users.noreply.github.com>
2023-09-17 16:10:35 +0200

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-read -p "node? " id
-source ~/config/spacemesh.sh $id
+if [ -z $1 ]
+  then 
+    echo "Running nodes:"
+    ps aux | grep go-spacemesh | grep -v grep | awk 'match($0, /spacemesh[0-9]|spacemesh[0-9][0-9]/) {print substr($0, RSTART, RLENGTH)}' | sed 's/spacemesh//g'
+    echo "------------------------"
+    read -p "Node?  " id
+    echo "------------------------"
+  else 
+    id=$1
+fi
+source ~/scripts/spacemesh/config/node$id
+
 pid=$(ps aux | grep spacemesh | grep "$port1" | awk '{print $2}')
 kill $pid
