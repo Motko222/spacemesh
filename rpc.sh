9d93ab6d1e23089ff761c03b5a0f713bb4fa14f3
Motko222 <77466238+Motko222@users.noreply.github.com>
2023-09-17 16:13:39 +0200

    Update rpc.sh

--- rpc.sh
+++ rpc.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-read -p "node? " id
-source ~/config/spacemesh.sh $id
+source ~/scripts/spacemesh/config/env
+if [ -z $1 ]
+  then 
+    echo "Running nodes:"
+    ps aux | grep go-spacemesh | grep -v grep | awk 'match($0, /spacemesh[0-9]|spacemesh[0-9][0-9]/) {print substr($0, RSTART, RLENGTH)}' | sed 's/spacemesh//g'
+    echo "------------------------"
+    read -p "Node?  " id
+    echo "------------------------"
+  else 
+    id=$1
+fi
+source ~/scripts/spacemesh/config/node$id
 
 cd $smbase
 
