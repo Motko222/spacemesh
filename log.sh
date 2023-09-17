6237f85e150255d5fe23d496568788d290324e26
Motko222 <77466238+Motko222@users.noreply.github.com>
2023-09-17 16:15:49 +0200

    Update log.sh

--- log.sh
+++ log.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-read -p "node? " id
+if [ -z $1 ]
+  then 
+    echo "Running nodes:"
+    ps aux | grep go-spacemesh | grep -v grep | awk 'match($0, /spacemesh[0-9]|spacemesh[0-9][0-9]/) {print substr($0, RSTART, RLENGTH)}' | sed 's/spacemesh//g'
+    echo "------------------------"
+    read -p "Node?  " id
+    echo "------------------------"
+  else 
+    id=$1
+fi
+
 tail -f ~/logs/spacemesh$id.log
