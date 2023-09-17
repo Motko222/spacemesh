4e2389596b9917a273af3796e1b28faafe6b55fa
Motko222 <77466238+Motko222@users.noreply.github.com>
2023-09-17 16:00:02 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
-source ~/config/spacemesh.sh
+source ~/scripts/spacemesh/config/env
 
 read -p "Sure?" c
  case $c in
  y)
-  echo "removing old files..."
-  cd $smbase
-  rm go-spacemesh libpost.so profiler
+  if [ -d $smbase ]
+    then
+      echo "Removing old files..."
+      cd $smbase
+      rm go-spacemesh libpost.so profiler
+    else
+      mkdir $smbase
+      cd $smbase
+  fi
   echo "Get latest release from here: https://github.com/spacemeshos/go-spacemesh/releases"
   read -p "URL? " url
-  echo "downloading..."
+  echo "Downloading..."
   wget $url
   unzip -j Linux.zip
-  echo "setting permissions..."
+  echo "Setting permissions..."
   chmod 777 go-spacemesh profiler
-  echo "deleting zip..."
+  echo "Deleting zip..."
   rm Linux.zip
-  echo "done."
+  echo "Done."
  ;;
  esac
