f9c05fb930b0e30f627ea4978ffbf1efac65bb1c
Motko222 <77466238+Motko222@users.noreply.github.com>
2023-10-03 12:34:40 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -11,12 +11,20 @@ if [ -z $1 ]
   else 
     id=$1
 fi
-source ~/scripts/spacemesh/config/node$id
 
+source ~/scripts/spacemesh/config/node$id
 cd $smbase
 pid=$(ps aux | grep spacemesh | grep "$port1" | awk '{print $2}')
-kill $pid
-sleep 5s
-echo "Starting node $id..."
+
+if [ -z $pid ]
+  then 
+    echo "Node $id is not running..."
+  else 
+    echo "Killing process $pid..."
+    kill $pid
+    sleep 5s
+fi
+
+echo "Starting node $id ($port1 $config $smdata $lock)..."
 ./go-spacemesh --listen /ip4/0.0.0.0/tcp/$port1 --config $config -d $smdata --filelock $lock > ~/logs/spacemesh$id.log 2>&1 &
 ps aux | grep spacemesh | grep "$port1"
