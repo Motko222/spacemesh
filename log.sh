5b8518d740df89b03a9bdb25bb00b86bf866ee7f
Motko222 <77466238+Motko222@users.noreply.github.com>
2023-12-21 09:54:14 +0100

    Update log.sh

--- log.sh
+++ log.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ -z $1 ]
+if [ -z $1 ] 
   then 
     echo "Running nodes:"
     ps aux | grep go-spacemesh | grep -v grep | awk 'match($0, /spacemesh[0-9]|spacemesh[0-9][0-9]/) {print substr($0, RSTART, RLENGTH)}' | sed 's/spacemesh//g'
